11be1efb3bf16f090be969b91737a8b82a1c4ba3
huykevin2002 <giahuy300336@gmail.com>
2023-03-29 00:09:24 +0700

    init admin panel

---
+++ admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">lamaadmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://img.freepik.com/premium-vector/p-letter-vector-logo-design-template-graphic-alphabet-symbol-corporate-business-identity_418020-122.jpg?w=2000" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
