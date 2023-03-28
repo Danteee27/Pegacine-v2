4bfcf0e3c58714b1e6ccd35746bd8d1f6d977c07
Danteee27 <duongducanh6101@gmail.com>
2023-03-28 18:04:14 +0700

    sql migrate db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'libs/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CountryModule } from './country/country.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CountryModule,
+    DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
