7af2be73b76675d763621e9f5aad42e00dc6e675
phunguyen58 <nvphu0508@gmail.com>
2023-03-29 11:19:51 +0700

    init homepage

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import Home from './pages/home/Home';
 export default function App() {
   return (
     <div>
-      <Login />
+      <Home />
     </div>
   );
 }
--- src/client/src/components/navbar/Navbar.jsx
+++ src/client/src/components/navbar/Navbar.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import "./navbar.scss"
-import Search from "@mui/icons-material/Search"
-import Notifications from "@mui/icons-material/Notifications"
+import Search from '@mui/icons-material/Search';
+import Notifications from '@mui/icons-material/Notifications';
+import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react';
+import './navbar.scss';
 
-function Navbar() {
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
-    <div>
-      <div className="navbar">
-        <div className="container">
-            <div className="left">
-                <img src="../logo.png" alt="" />
-                <span>Homepage</span>
-                <span>Series</span>
-                <span>Movies</span>
-                <span>New and popular</span>
-                <span>My list</span>
-                </div>
-                <div className='right'>
-                    <Search/>
-                    Phu Nguyen
-                    <Notifications/>
-                </div>
+    <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
+      <div className="container">
+        <div className="left">
+          <img src="../logo.png" alt="" />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span className="username">Phu Nguyen</span>
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-)
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
