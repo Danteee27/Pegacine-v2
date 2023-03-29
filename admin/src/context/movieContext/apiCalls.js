43af8aac3d26d702bbbde5cbd2aa304802868cad
huykevin2002 <giahuy300336@gmail.com>
2023-03-29 12:25:18 +0700

    admin dashboard get and delete operations

---
+++ admin/src/context/movieContext/apiCalls.js
@@ -0,0 +1,22 @@
+import { deleteMovieFailure, deleteMovieStart, deleteMovieSuccess, getMoviesFailure, getMoviesStart, getMoviesSuccess } from "./MovieActions"
+import axios from "axios";
+
+export const getMovies = async (dispatch) => {
+    dispatch(getMoviesStart());
+    try {
+        const res = await axios.get("/movies"); // access token update later
+        dispatch(getMoviesSuccess(res.data))
+    } catch (error) {
+        dispatch(getMoviesFailure());
+    }
+};
+
+export const deleteMovie = async (id, dispatch) => {
+    dispatch(deleteMovieStart());
+    try {
+        await axios.delete("/movies" + id); // access token update later
+        dispatch(deleteMovieSuccess(id))
+    } catch (error) {
+        dispatch(deleteMovieFailure());
+    }
+};
