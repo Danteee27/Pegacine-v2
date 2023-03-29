3a247865406365d903921a41d5bceeef218ba3a5
phunguyen58 <nvphu0508@gmail.com>
2023-03-29 10:44:56 +0700

    complete Login page

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 export default function App() {
   return (
     <div>
-      <Register />
+      <Login />
     </div>
   );
 }
--- src/client/src/pages/login/Login.jsx
+++ src/client/src/pages/login/Login.jsx
@@ -1,22 +1,18 @@
-import "./login.scss";
+import './login.scss';
 
 export default function Login() {
   return (
     <div className="login">
       <div className="top">
         <div className="wrapper">
-          <img
-            className="logo"
-            src="../logo.png"
-            alt=""
-          />
+          <img className="logo" src="../logo.png" alt="" />
         </div>
       </div>
       <div className="container">
         <form>
           <h1>Sign In</h1>
-          <input type="email" placeholder="Email or phone number" />
-          <input type="password" placeholder="Password" />
+          <input type="email" placeholder="Enter your email address" />
+          <input type="password" placeholder="Enter your password" />
           <button className="loginButton">Sign In</button>
           <span>
             New to Netflix? <b>Sign up now.</b>
