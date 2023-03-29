197c7285199f8cb97c21fb469b3f073f40a842a3
phunguyen58 <nvphu0508@gmail.com>
2023-03-29 08:48:13 +0700

    init register page

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import Navbar from './components/navbar/Navbar';
+import Register from './pages/register/Register';
 export default function App() {
   return (
     <div>
-      <Navbar />
+      <Register />
     </div>
   );
 }
