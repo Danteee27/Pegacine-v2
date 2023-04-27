166b6d1e7e1abec8731d883bbe7e1be39819cc56
Nguyen Ngoc Anh Khoa <73281000+anhkhoajjo@users.noreply.github.com>
2023-04-27 12:21:23 +0700

    Update Test.jsx

--- src/client/src/pages/test/Test.jsx
+++ src/client/src/pages/test/Test.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Navbar from "../../components/navbar/Navbar";
-
-function TestPage() {
+import './test.css'
+function TestPage({testValue}) {
     const [job, setJob] = useState('');
     const [jobs, setJobs] = useState(["con ga", "con bo ", "con cuu"]);
     useEffect(() => {
@@ -10,11 +10,15 @@ function TestPage() {
         }
     );
 
+    console.log("url:",testValue)
+
 
     return (
         <div style={{padding: 32}}>
             <Navbar/>
             <h1>Hello world</h1>
+            <h1>Helloo: {testValue}</h1>
+            <h1>type: {typeof testValue}</h1>
             <input
                 value={job}
                 onChange={(e) => {
