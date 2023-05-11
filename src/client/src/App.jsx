042e71fe0fdef0917825ec3ef41bf5822ff37c81
phunguyen58 <nvphu0508@gmail.com>
2023-05-11 18:49:48 +0700

    route

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -7,79 +7,99 @@ import SearchPage from './pages/search/SearchPage';
 import PlayerPage from './pages/player/PlayerPage';
 import MyList from './pages/myList/MyList';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-    Routes,
-    useLocation,
-    useSearchParams,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Routes,
+  useLocation,
+  useSearchParams,
 } from 'react-router-dom';
 
-import {useContext} from 'react';
+import { useContext } from 'react';
 import TestPage from './pages/test/Test';
 import Series from './pages/series/Series';
 import Test from './pages/test/Test';
-import {useEffect} from 'react';
-import {useState} from 'react';
-import {RequireAuth} from 'react-auth-kit';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { RequireAuth } from 'react-auth-kit';
+import PlanForm from './pages/planForm/PlanForm';
+import PaymentPicker from './pages/paymentPicker/paymentPicker';
+import PaymentConfirm from './pages/paymentConfirm/paymentConfirm';
 
 export default function App() {
-    // const location = useLocation();
-    // const queryParameters = new URLSearchParams(location.search);
-    const [searchParams, setSearchParams] = useSearchParams();
+  // const location = useLocation();
+  // const queryParameters = new URLSearchParams(location.search);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    return (
-        <div>
-            <Routes>
-                <Route
-                    path="/home"
-                    element={
-                        <RequireAuth loginPath="/login">
-                            <Home/>
-                        </RequireAuth>
-                    }
-                />
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route
-                    path="/player/*"
-                    element={
-                        <RequireAuth loginPath="/login">
-                            <PlayerPage/>
-                        </RequireAuth>
-                    }
-                />
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <RequireAuth loginPath="/login">
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/player/*"
+          element={
+            <RequireAuth loginPath="/login">
+              <PlayerPage />
+            </RequireAuth>
+          }
+        />
 
-                <Route
-                    path="/test"
-                    element={<TestPage testValue={searchParams.get('url')}/>}
-                />
-                <Route
-                    path="/myList"
-                    element={
-                        <RequireAuth loginPath="/login">
-                            <MyList fetchUrl={requests.fetchTrending}/>
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="/search"
-                    element={
-                        <RequireAuth loginPath="/login">
-                            <SearchPage/>
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="/series"
-                    element={
-                        <RequireAuth loginPath="/login">
-                            <Series fetchUrl={requests.fetchTrending}/>
-                        </RequireAuth>
-                    }
-                />
-            </Routes>
-        </div>
-    );
+        <Route
+          path="/test"
+          element={<TestPage testValue={searchParams.get('url')} />}
+        />
+        <Route
+          path="/myList"
+          element={
+            <RequireAuth loginPath="/login">
+              <MyList fetchUrl={requests.fetchTrending} />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <RequireAuth loginPath="/login">
+              <SearchPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/series"
+          element={
+            <RequireAuth loginPath="/login">
+              <Series fetchUrl={requests.fetchTrending} />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/plan-form"
+          element={
+            <RequireAuth loginPath="/login">
+              <PlanForm fetchUrl={requests.fetchTrending} />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/payment-picker"
+          element={
+            <RequireAuth loginPath="/login">
+              <PaymentPicker fetchUrl={requests.fetchTrending} />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
